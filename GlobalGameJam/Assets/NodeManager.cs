4771729e02d3e831ef44631b5718787e052e770c
Neville King <nevster07@gmail.com>
2016-01-30 00:54:24 +0000

    Started travesing the nodes in the xml file

--- GlobalGameJam/Assets/NodeManager.cs
+++ GlobalGameJam/Assets/NodeManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml;
 
 public class NodeManager : MonoBehaviour
 {
@@ -12,18 +13,31 @@ public class NodeManager : MonoBehaviour
         DialogueNode currentNode = new DialogueNode();
         head = currentNode;
 
-        for(int i = 0; i < 5; i++)
+        // Read in the XML file
+        XmlTextReader reader = new XmlTextReader("Assets/Dialogue/Dialogue.xml");
+
+        XmlDocument xml = new XmlDocument();
+        xml.Load(reader);
+        XmlNodeList sections = xml.SelectNodes("/dialogue/dialogueSection");
+
+        foreach (XmlNode dSection in sections)
         {
-            currentNode.dialogueText = "First words. Hooray! " + i;
-            currentNode.nextNode.Add(new DialogueNode());
-            currentNode = currentNode.nextNode[0];
+            Debug.Log(dSection["text"].InnerText);
         }
 
-        currentNode = head;
-        while (currentNode.nextNode.Count != 0)
-        {
-            Debug.Log(currentNode.dialogueText);
-            currentNode = currentNode.nextNode[0];
-        }
+
+        //for(int i = 0; i < 5; i++)
+        //{
+        //    currentNode.dialogueText = "First words. Hooray! " + i;
+        //    currentNode.nextNode.Add(new DialogueNode());
+        //    currentNode = currentNode.nextNode[0];
+        //}
+
+        //currentNode = head;
+        //while (currentNode.nextNode.Count != 0)
+        //{
+        //    Debug.Log(currentNode.dialogueText);
+        //    currentNode = currentNode.nextNode[0];
+        //}
     }
 }
