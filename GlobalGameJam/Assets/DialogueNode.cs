ed946169dfc064f812d27f9a1f55619750edd6a4
AdamDWalker <adamwalker03@googlemail.com>
2016-01-30 00:04:53 +0000

    Working linked list of nodes

---
+++ GlobalGameJam/Assets/DialogueNode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class DialogueNode
+{
+    // How long the text will show for
+    public float time = 5.0f;
+
+    // The line of text this node is storing
+    public string dialogueText;
+
+    public int choiceCount = 1;
+
+    //DialogueNode[] nextNode = new DialogueNode[3];
+
+    public List<DialogueNode> nextNode = new List<DialogueNode>();
+
+}
---
+++ GlobalGameJam/Assets/NodeManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeManager : MonoBehaviour
+{
+    
+    public DialogueNode head;
+
+    public NodeManager()
+    {
+        // Create a node and populate the head value
+        DialogueNode currentNode = new DialogueNode();
+        head = currentNode;
+
+        for(int i = 0; i < 5; i++)
+        {
+            currentNode.dialogueText = "First words. Hooray! " + i;
+            currentNode.nextNode.Add(new DialogueNode());
+            currentNode = currentNode.nextNode[0];
+        }
+
+        currentNode = head;
+        while (currentNode.nextNode.Count != 0)
+        {
+            Debug.Log(currentNode.dialogueText);
+            currentNode = currentNode.nextNode[0];
+        }
+    }
+}
